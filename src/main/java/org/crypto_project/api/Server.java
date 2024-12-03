b2a04f523d2f5b9785d3baa2f453d4d59c8a4497
Castellani-Josue <josue.castellani@student.hepl.be>
2024-12-03 15:44:07 +0100

    gfbf

--- src/main/java/org/crypto_project/api/Server.java
+++ src/main/java/org/crypto_project/api/Server.java
@@ -37,7 +37,7 @@ public class Server
             }
             char[] password = properties.getProperty("keystore.password").toCharArray();
 
-            
+
             KeyStore ks = KeyStore.getInstance("JKS");
             try (FileInputStream fis = new FileInputStream("keystore.jks")) {
                 ks.load(fis, password);
